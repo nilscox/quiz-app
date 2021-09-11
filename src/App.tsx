68a9d7c74f4a2bf2d2395207994e376497b85435
Nils <nils@epita.fr>
2021-09-11 14:07:08 +0200

    remove react imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,3 @@
-import React from 'react';
-
 const App: React.FC = () => <>app</>;
 
 export default App;
--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,15 @@
 /* eslint-disable */
 
 const path = require('path');
-const { HotModuleReplacementPlugin } = require('webpack');
+const { EnvironmentPlugin, HotModuleReplacementPlugin, ProvidePlugin } = require('webpack');
 const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 const { NODE_ENV = 'development', HOST = '0.0.0.0', PORT = '8000' } = process.env;
-const dev = NODE_ENV === 'development';
 
-module.exports = {
-  mode: dev ? 'development' : 'production',
+/** @type {import('webpack').Configuration} */
+const config = (module.exports = {
+  mode: 'development',
   devtool: 'source-map',
 
   entry: './src/index.tsx',
@@ -37,14 +37,24 @@ module.exports = {
   },
 
   plugins: [
-    dev && new HotModuleReplacementPlugin(),
-    dev && new ReactRefreshWebpackPlugin(),
+    //
+    new EnvironmentPlugin(),
+    new ProvidePlugin({ React: 'react' }),
     new HtmlWebpackPlugin(),
-  ].filter(Boolean),
+  ],
+});
 
-  devServer: {
+if (NODE_ENV === 'development') {
+  config.plugins.push(new HotModuleReplacementPlugin());
+  config.plugins.push(new ReactRefreshWebpackPlugin());
+
+  config.devServer = {
     host: HOST,
     port: Number(PORT),
     hot: true,
-  },
-};
+  };
+}
+
+if (NODE_ENV === 'production') {
+  config.mode = 'production';
+}
